48e21493c5b7feee71f1c3423e10446cf15da3d2
Tay Gee Yong <53674303+geeyong1115@users.noreply.github.com>
2022-04-10 20:58:43 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -156,5 +156,137 @@ def GetAttOutPut():
 
     return render_template('GetAttOutPut.html', results=lresults,)
 
+@app.route("/addemp", methods=['POST'])
+def AddEmp():
+    empid = request.form['empid']
+    name = request.form['name']
+    gender = request.form['gender']
+    phone = request.form['phone']
+    location = request.form['location']
+    rate_per_day = request.form['rate_per_day']
+    position = request.form['position']
+    hire_date = request.form['hire_date']
+    image = request.files['image']
+    print(type(hire_date))
+    conv_rate = int(rate_per_day)
+    # print(int(hire_date))
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    # if image.filename == "":
+    #     return "Please select a file"
+
+    try:
+
+        cursor.execute(insert_sql, (empid, name, gender, phone,
+                       location, conv_rate, position, hire_date))
+        db_conn.commit()
+        # Uplaod image file in S3 #
+        image_name_in_s3 = "empid-" + str(empid) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(
+                Key=image_name_in_s3, Body=image)
+            bucket_location = boto3.client(
+                's3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                image_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('AddEmpOutput.html', name=name)
+
+
+@app.route("/getemp", methods=['GET', 'POST'])
+def getEmp():
+    return render_template('GetEmp.html')
+
+
+@app.route("/update/<empid>", methods=['GET', 'POST'])
+def updateEmp(empid):
+
+    cursor = db_conn.cursor()
+    cursor.execute('Select * from employee WHERE empid = %s', empid)
+    results = cursor.fetchall()
+    cursor.close()
+
+    return render_template('Edit.html', results=results)
+
+
+@app.route("/triggerUpdate", methods=['GET', 'POST'])
+def triggerUpdate():
+
+    empid = request.form['empid']
+    name = request.form['name']
+    gender = request.form['gender']
+    phone = request.form['phone']
+    location = request.form['location']
+    rate_per_day = request.form['rate_per_day']
+    position = request.form['position']
+    hire_date = request.form['hire_date']
+
+    print(type(rate_per_day))
+
+    cursor = db_conn.cursor()
+    cursor.execute(
+        'UPDATE employee SET name=%s,gender=%s,phone=%s,location=%s,rate_per_day=%s,position=%s,hire_date=%s  WHERE empid=%s',
+        (name,
+         gender,
+         phone,
+         location,
+         int(rate_per_day),
+         position,
+         hire_date,
+         empid
+         )
+    )
+    db_conn.commit()
+    cursor.close()
+    print(cursor.rowcount, "record(s) affected")
+
+    return render_template('GetEmpOutput.html')
+
+
+@app.route("/delete/<empid>", methods=['GET'])
+def deleteEmp(empid):
+    cursor = db_conn.cursor()
+    cursor.execute('DELETE FROM employee WHERE empid = %s', empid)
+    db_conn.commit()
+    cursor.close()
+    time.sleep(2)
+    return render_template('GetEmpOutput.html')
+
+
+@app.route("/fetchdata", methods=['GET', 'POST'])
+def FetchData():
+    cursor = db_conn.cursor()
+    cursor.execute('Select * from employee')
+    results = cursor.fetchall()
+    lresults = list(results)
+    cursor.close()
+
+    return render_template(
+        'GetEmpOutput.html',
+        results=lresults,
+    )
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
